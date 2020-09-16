d2edab57bf46ebd0a50a901cb777972dd2d77e4d
MainIT <67462191+MainIT@users.noreply.github.com>
2020-09-16 10:19:24 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+$ pkg update && pkg upgrade
+
+$ pkg install git
+
+$ pkg install python2
+
+$ pip install requests
+
+$ pip install bs4
+
+$ git clone https://github.com/MainIT/IG_FolLik
+
+$ cd IG_FolLik
+
+$ sh install.sh
+
+$ python Insta_FoLik.py
